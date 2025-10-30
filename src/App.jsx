a62cc6797e37ba442f9622af85cb32319719183f
Zeeshan Khan <zeeshan.saeed8323@gmail.com>
2025-10-30 21:18:31 +0500

    Add Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './component/Navbar';
+import Footer from './component/Footer';
 
 
 function App() {
   return (
-    <>
+    <> 
+      
       <Navbar />
+      <Footer />
     </>
   );
 }
---
+++ src/component/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css'; // (optional if you plan to style it)
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2024 My React App. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
