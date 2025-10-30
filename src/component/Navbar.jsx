a8ab193210020ade2702f33a1c66a7358f319e63
Zeeshan Khan <f2023266847@umt.edu.pk>
2025-10-30 12:05:10 +0500

    Pushing my first React page folder

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,16 @@
+import "../style/Navbar.css"
+
+const Navbar = () => {
+    
+return <p> Navbar</p>
+
+
+}
+export default Navbar 
+
+
+// must function start with file name 
+// file name start with capital 
+// and function must shoiuld return some html
+
+
